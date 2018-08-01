db9f5696d7fe1569d2b87c6e3455c55d4eb11e00
Sean Lucius <seanlucius22@icloud.com>
2018-08-01 03:21:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name) {
-  return [kittens.push(name)];
+  return kittens.push(name);
 }
 
 function destructivelyPrependKitten(name) {
